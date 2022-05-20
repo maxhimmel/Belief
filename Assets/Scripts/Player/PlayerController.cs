3e3ce0314992eb31fb7190bac33c4abed1e5a0cf
Max <himmelmax@gmail.com>
2022-05-20 11:28:12 -0700

    player moves along proper plane
    (relative to camera)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Zenject;
 using MaulGrab.Gameplay.Weapons;
 using MaulGrab.Gameplay.Pickups;
+using Cinemachine;
 
 namespace MaulGrab.Gameplay.Player
 {
@@ -12,30 +13,35 @@ namespace MaulGrab.Gameplay.Player
 		private Rewired.Player _input;
 		private CharacterMotor _motor;
 		private Gun _gun;
+		private CinemachineBrain _cineBrain;
 
 		[Inject]
 		public void Construct( 
 			Rewired.Player input,
 			CharacterMotor motor,
-			Gun gun)
+			Gun gun,
+			CinemachineBrain cineBrain)
 		{
 			_input = input;
 			_motor = motor;
 			_gun = gun;
+			_cineBrain = cineBrain;
 		}
 
 		private void Update()
 		{
-			Vector2 moveInput = GetMoveDirection();
+			Vector3 moveInput = GetMoveDirection();
 			_motor.SetDesiredVelocity( moveInput );
 
 			HandleGunInput();
 		}
 
-		private Vector2 GetMoveDirection()
+		private Vector3 GetMoveDirection()
 		{
-			Vector3 moveInput = _input.GetAxis2D( ReConsts.Action.Horizontal, ReConsts.Action.Vertical );
-			return Vector2.ClampMagnitude( moveInput, 1 );
+			Vector3 moveInput = new Vector3( _input.GetAxis( ReConsts.Action.Horizontal ), 0, _input.GetAxis( ReConsts.Action.Vertical ) );
+			moveInput = Quaternion.Euler( 0, _cineBrain.transform.eulerAngles.y, 0 ) * moveInput;
+
+			return Vector3.ClampMagnitude( moveInput, 1 );
 		}
 
 		private void HandleGunInput()
