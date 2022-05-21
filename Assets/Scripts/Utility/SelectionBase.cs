c64371cb1c80ed3705526da0ea8f5c8a1f773ce2
Max <himmelmax@gmail.com>
2022-05-20 19:20:12 -0700

    created helper for selecting desired gameobjects from nested hierarchy

---
+++ Assets/Scripts/Utility/SelectionBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaulGrab.Utility
+{
+    [SelectionBase]
+    public class SelectionBase : MonoBehaviour
+    {
+    }
+}
