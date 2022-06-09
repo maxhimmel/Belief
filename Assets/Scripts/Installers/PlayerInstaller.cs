acf875d05906d0337aa7499d8e99aba320bd2def
Max <himmelmax@gmail.com>
2022-06-08 18:07:30 -0700

    binding aim assist to player installer

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using MaulGrab.Gameplay;
 using MaulGrab.Gameplay.Movement;
 using MaulGrab.Gameplay.Utility;
 using MaulGrab.Gameplay.Weapons;
@@ -9,15 +8,34 @@ using Zenject;
 namespace MaulGrab.Installers
 {
     public class PlayerInstaller : MonoInstaller
-    {
+	{
+		[SerializeField] private CollisionQueryData _aimAssistData = default;
+
 		public override void InstallBindings()
 		{
 			Rigidbody body = GetComponent<Rigidbody>();
 
+			var body = GetComponent<Rigidbody>();
 			Container.Bind<IRigidbody>().To<Rigidbody3D>().AsSingle().WithArguments( body );
+
+			BindAimAssist();
+
 			Container.Bind<CharacterMotor>().FromComponentOnRoot().AsSingle();
 
 			Container.Bind<Gun>().FromComponentInChildren().AsSingle();
+		}
+
+		private void BindAimAssist()
+		{
+			var visionBounds = GetComponentInChildren<VisionBounds>();
+			Container.BindInstance<VisionBounds>( visionBounds ).AsSingle();
+
+			Container.Bind<IGameObjectProvider>()
+				.To<SphereOverlapProvider>()
+				.AsSingle().WithArguments( _aimAssistData );
+
+			var aimAssist = GetComponentInChildren<AimAssist>();
+			Container.BindInstance<AimAssist>( aimAssist ).AsSingle();
 		}
 	}
 }
