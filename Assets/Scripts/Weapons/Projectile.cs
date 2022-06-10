65f8d79b8c660999236a1b6041ac1d6c17f341f1
Max <himmelmax@gmail.com>
2022-06-09 17:28:58 -0700

    projectile can now deal damage!

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -6,15 +6,33 @@ namespace MaulGrab.Gameplay.Weapons
 {
     public class Projectile : MonoBehaviour, IExpiry
     {
+		[SerializeField] private DamageEvent _damageEvent = new DamageEvent();
+
+		private Transform _owner;
 		private IRigidbody _body;
+		private HitBoxService _hitBoxService;
 
 		[Inject]
-		public void Construct( IRigidbody body )
+		public void Construct( [Inject(Id = InstallerID.Owner)] Transform owner,
+			IRigidbody body,
+			HitBoxService hitBoxService )
 		{
+			_owner = owner;
             _body = body;
+
+			_hitBoxService = hitBoxService;
+			_hitBoxService.NotifyHit += OnHit;
 		}
 
-        public void Fire( Vector3 velocity, float torque )
+		private void OnHit( object hitBox, IDamageable victim )
+		{
+			var newDMG = new DamageEvent( _damageEvent );
+			newDMG.HitDirection = (victim.Transform.position - _body.Position).normalized;
+
+			victim.TakeDamage( newDMG, _owner, _body.Transform );
+		}
+
+		public void Fire( Vector3 velocity, float torque )
 		{
 			_body.AddForce( velocity, ForceType.Impulse );
 			_body.AddTorque( _body.Transform.up * torque, ForceType.Impulse );
