322b6f4b3dc9b629c8091d89013a2596baceafe0
Max <himmelmax@gmail.com>
2022-06-09 17:29:43 -0700

    skateboard handles deactivating hit boxes once collided
    also turns off attractor explicitly to account for new proper composite colliders

--- Assets/Scripts/Weapons/Skateboard.cs
+++ Assets/Scripts/Weapons/Skateboard.cs
@@ -12,17 +12,22 @@ namespace MaulGrab.Gameplay.Weapons
 		[SerializeField] private float _sleepLinearDrag = 1;
 		[BoxGroup( "On Collided" )]
 		[SerializeField] private float _sleepAngularDrag = 1;
+		[BoxGroup( "On Collided" )]
+		[SerializeField] private ForceAttractor _attractor = default;
 
 		private IRigidbody _body;
 		private List<Collider> _colliders;
-		private LayerMask _collectableLayer = 0;
+		private HitBoxService _hitBoxService;
+		private int _collectableLayer = 0;
 
 		[Inject]
 		public void Construct( IRigidbody body,
-			List<Collider> collider )
+			List<Collider> collider,
+			HitBoxService hitBoxService )
 		{
 			_body = body;
 			_colliders = collider;
+			_hitBoxService = hitBoxService;
 		}
 
 		private void OnCollisionEnter( Collision collision )
@@ -36,6 +41,9 @@ namespace MaulGrab.Gameplay.Weapons
 				collider.isTrigger = true;
 				collider.gameObject.layer = _collectableLayer;
 			}
+
+			_attractor.gameObject.SetActive( true );
+			_hitBoxService.DeactivateHitBoxes();
 		}
 
 		private void Awake()
