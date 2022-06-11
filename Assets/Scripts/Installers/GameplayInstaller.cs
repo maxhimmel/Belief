16cfcf1b14c519fe97de51f993848accf8b04346
Max <himmelmax@gmail.com>
2022-06-11 15:29:32 -0700

    added SUPER CRAPPY way of getting death vfx
    this is way to generic and needs to be placed into a bespoke service that can then be bound

--- Assets/Scripts/Installers/GameplayInstaller.cs
+++ Assets/Scripts/Installers/GameplayInstaller.cs
@@ -9,10 +9,15 @@ namespace MaulGrab.Installers
     public class GameplayInstaller : MonoInstaller
     {
         [SerializeField] private CinemachineBrain _gameplayCameraBrain = default;
+        [SerializeField] private ParticleSystem _deathVfx = default;
 
         public override void InstallBindings()
         {
             Container.BindInstance( _gameplayCameraBrain ).AsSingle();
+
+            // TODO: Create proper death fx service ...
+            Container.BindFactory<ParticleSystem, PlaceholderFactory<ParticleSystem>>()
+                .FromComponentInNewPrefab( _deathVfx );
         }
     }
 }
